cba5ae9e01ec490d147f24d0fc87df8539611871
artermi <ericcheng230@gmail.com>
2024-06-14 16:31:46 +0100

    main

--- main.cpp
+++ main.cpp
@@ -35,7 +35,7 @@ int main(int argc, char** argv){
 	srand(time(NULL));
 
 
-	for(double r = 3.0; r < 5.01; r += 0.1 ){
+	for(double r = 3.0; r < 5.01; r += 0.5 ){
 		for(double b = 0; b < 1.01; b += 0.1){
 			for (double g = 0.0; g < 1.01; g += 0.1){
 					do_once(r,b,g,0);
