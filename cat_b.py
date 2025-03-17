d10d74b2f48565a369b795abfdef4454ff9d7877
artermi <ericcheng230@gmail.com>
2025-03-17 23:24:22 +0000

    modify beta capture

--- cat_b.py
+++ cat_b.py
@@ -5,23 +5,22 @@ import matplotlib.pyplot as plt
 from collections import defaultdict
 from scipy.ndimage import gaussian_filter1d
 from matplotlib import rc
+import tikzplotlib
 
 
-def remove_outliers_and_average(numbers, method="iqr", threshold=1.5):
+def remove_outliers(numbers, method="iqr", threshold=1.5):
     if not numbers:
-        return None  # Handle empty list case
+        return []
 
     numbers = np.array(numbers)
 
     if method == "iqr":
-        # Use the interquartile range (IQR) method
         Q1, Q3 = np.percentile(numbers, [25, 75])
         IQR = Q3 - Q1
         lower_bound = Q1 - threshold * IQR
         upper_bound = Q3 + threshold * IQR
 
     elif method == "std":
-        # Use the standard deviation method
         mean = np.mean(numbers)
         std_dev = np.std(numbers)
         lower_bound = mean - threshold * std_dev
@@ -30,23 +29,20 @@ def remove_outliers_and_average(numbers, method="iqr", threshold=1.5):
     else:
         raise ValueError("Invalid method. Choose 'iqr' or 'std'.")
 
-    # Filter the numbers within bounds
-    filtered_numbers = numbers[(numbers >= lower_bound) & (numbers <= upper_bound)]
-
-    if len(filtered_numbers) == 0:
-        return None  # Avoid division by zero if all numbers are outliers
-
-    return np.mean(filtered_numbers)
+    return numbers[(numbers >= lower_bound) & (numbers <= upper_bound)]
 
 
-D = []
-C = []
-SC = []
-SC2 = []
+def mean_and_error(numbers):
+    if len(numbers) == 0:
+        return None, None
+    mean = np.mean(numbers)
+    error = np.std(numbers)
+    return mean, error
 
-AVG = []
+
+D, C, SC, SC2 = [], [], [], []
+D_err, C_err, SC_err, SC2_err = [], [], [], []
 b = []
-
 
 d_name = '.'
 t_name = 'new_b.tex'
@@ -54,80 +50,58 @@ if len(sys.argv) > 2:
     d_name = sys.argv[1]
     t_name = sys.argv[2]
 
-for root,dirs,files in sorted(os.walk(d_name,topdown = False)):
-
+for root, dirs, files in os.walk(d_name, topdown=True):
     for name in sorted(files):
-        if name.find('.dat') >= 0 and name.find('avg') < 0:
-            # r_0200_b_0050_g_0020_mod_00.dat
-            # 0123456789|123456789|1234567
-            bb = float(name[9:13])/100
+        if name.endswith('.dat') and 'avg' not in name:
+            bb = float(name.split('_')[3]) / 1000
 
-            Final_D = []
-            Final_C = []
-            FinalSC = []
-            FinalSC2 = []
+            Final_D, Final_C, FinalSC, FinalSC2 = [], [], [], []
             with open(os.path.join(root, name)) as file:
-                for last_line in file:
-                    nmbs = last_line.split()
+                for line in file:
+                    nmbs = line.split()
                     Final_D.append(float(nmbs[1]))
                     Final_C.append(float(nmbs[2]))
                     FinalSC.append(float(nmbs[3]))
                     FinalSC2.append(float(nmbs[4]))
 
-#                   if int(nmbs[0]) == 10000:
-#                       break
-
-
             b.append(bb)
-            D.append(remove_outliers_and_average(Final_D))
-            C.append( remove_outliers_and_average(Final_C) )
-            SC.append( remove_outliers_and_average(FinalSC) ) 
-            SC2.append( remove_outliers_and_average(FinalSC2) ) 
-#            D.append( sum(Final_D[-100:])/100 )
-#            C.append( sum(Final_C[-100:])/100 )
-#            SC.append( sum(FinalSC[-100:])/100 ) 
-#            SC2.append( sum(FinalSC2[-100:])/100 ) 
 
-        if name.find('.dat') >= 0 and name.find('avg') > 0:
-            with open(os.path.join(root, name)) as file:
-                for last_line in file:
-                    pass
-            AVG.append(float(last_line))
+            filt_D = remove_outliers(Final_D)
+            filt_C = remove_outliers(Final_C)
+            filt_SC = remove_outliers(FinalSC)
+            filt_SC2 = remove_outliers(FinalSC2)
 
-PS = [100 for i in b]
+            mean_D, err_D = mean_and_error(filt_D)
+            mean_C, err_C = mean_and_error(filt_C)
+            mean_SC, err_SC = mean_and_error(filt_SC)
+            mean_SC2, err_SC2 = mean_and_error(filt_SC2)
+
+            D.append(mean_D)
+            C.append(mean_C)
+            SC.append(mean_SC)
+            SC2.append(mean_SC2)
+
+            D_err.append(err_D)
+            C_err.append(err_C)
+            SC_err.append(err_SC)
+            SC2_err.append(err_SC2)
 
 ax = plt.subplot(111)
 box = ax.get_position()
-ax.set_position([box.x0, box.y0 + box.height * 0.2,
-                 box.width, box.height * 0.8])
-#ax.plot(itr, L, color='#cb4335', marker= '.', label = 'Fixed defector')
+ax.set_position([box.x0, box.y0, box.width, box.height * 0.8])
 
-#D = gaussian_filter1d(D,.5)
-#C = gaussian_filter1d(C,.5)
-#SC = gaussian_filter1d(SC,.5)
-#SC2 = gaussian_filter1d(SC2,.5)
+ax.errorbar(b, D, yerr=D_err, fmt='.-', color='red', markersize=2, capsize=3)
+ax.errorbar(b, C, yerr=C_err, fmt='.-', color='blue', markersize=2, capsize=3)
+ax.errorbar(b, SC, yerr=SC_err, fmt='.-', color='green', markersize=2, capsize=3)
+ax.errorbar(b, SC2, yerr=SC2_err, fmt='.-', color='magenta', markersize=2, capsize=3)
 
-ax.plot(b, D, color='red', marker= '.',label = 'D',markersize=2)
-ax.plot(b, C, color='blue', marker= '.',label = 'C',markersize=2)
-ax.plot(b, SC, color='green', marker= '.',label = r'$P_1$',markersize=2)
-ax.plot(b, SC2, color='magenta', marker= '.',label = r'$P_2$',markersize=2)
+# Explicitly setting legend labels here to avoid tikzplotlib issues
+ax.legend(['D', 'C', r'$P_1$', r'$P_2$'], loc='upper center', bbox_to_anchor=(0.5, -0.1),
+          fancybox=True, shadow=True, ncol=4)
 
-#ax.plot(b, PS, color='magenta', marker= '.',label = 'Average',markersize=2)
-
-ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.2), fancybox=True, shadow=True, ncol=4)
-
-
-#ax2 = ax.twinx()
-#ax2.plot(b,AVG,color='magenta', marker= '.',label = 'Average',markersize=2)
-
-ax.set_ylim([-0.1,1.1])
-#ax2.set_ylim([2.5,3])
+ax.set_ylim([-0.1, 1.1])
 ax.set_xlabel(r'$\beta$')
 ax.set_ylabel('Fractions')
-#ax2.set_ylabel('Earnings')
 
 plt.grid(False)
-
-import tikzplotlib
-
 tikzplotlib.save(t_name)
